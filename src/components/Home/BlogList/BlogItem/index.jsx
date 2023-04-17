0c94a59a91d9c6a5bf415ede4f37344915040a5c
Vincent Rabate <118382789+VincentRabate@users.noreply.github.com>
2023-04-17 17:20:29 +0200

    third

--- src/components/Home/BlogList/BlogItem/index.jsx
+++ src/components/Home/BlogList/BlogItem/index.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import Chip from "../../../common/chip";
 import "./style.css"
 
-const BlogItem = (blog) => {
-  return (
-    <div>BLog Item</div>
-  )
-}
+const BlogItem = ({
+    blog:{
+    id,
+    description,
+    title,
+    createdAt,
+    authorName,
+    authorAvatar,
+    category,
+    cover,
+    },
+}) =>  ( <div className='blogItem-wrap'>
+        <img src={cover} alt="cover" /> 
+        <Chip label={category} />
+        <h3>{title}</h3>
+        <p className='blogItem-desc'>{description}</p>
+
+        <footer>
+            <div className='blogItem-author'>
+                <img src={authorAvatar} alt="avatar" />
+                <div>
+                    <h6>{authorName}</h6>
+                    <p>{createdAt}</p>
+                </div>
+            </div>
+            <Link to={`/blog/${id}`}> ➝ </Link>
+        </footer>
+    </div> 
+  );
 
 export default BlogItem
--- src/components/Home/BlogList/index.jsx
+++ src/components/Home/BlogList/index.jsx
@@ -1,12 +1,28 @@
+// import React from 'react';
+// import BlogItem from './BlogItem';
+// import "./style.css"
+
+// const BlogList = ({blogs}) => (
+//     <div className='blogList-wrap'>
+//         {blogs.map(blog=>
+//         <BlogItem key={blog.id} blog={blog.id} />)}
+//         </div>
+//   )
+
+// export default BlogList
+
 import React from 'react';
 import BlogItem from './BlogItem';
-import "./style.css"
+import './style.css';
 
-const BlogList = ({blogs}) => (
+const BlogList = ({ blogs }) => {
+  return (
     <div className='blogList-wrap'>
-        {blogs.map(blog=>
-        <BlogItem blog={blog.id} />)}
-        </div>
-  )
+      {blogs.map((blog) => (
+        <BlogItem blog={blog} />
+      ))}
+    </div>
+  );
+};
 
-export default BlogList
+export default BlogList;
